0343d726b39c25976234aa43918f75b9b0eee049
louistam888 <louiscodes888@gmail.com>
2022-10-20 21:15:53 -0400

    added fade-in effects for projects section. Added animated arrow in header, fade-in for nav and replaced header background image.

---
+++ javascript/fadeInEffect.js
@@ -0,0 +1,18 @@
+function scrollReveal() {
+	const revealPoint = 150;
+	const revealElement = document.querySelectorAll(".projectLeft, .projectRight");
+	for (let i = 0; i < revealElement.length; i++) {
+		const windowHeight = window.innerHeight;
+		const revealTop = revealElement[i].getBoundingClientRect().top;
+		if (revealTop < windowHeight - revealPoint) {
+			revealElement[i].classList.add("active");
+		} else {
+			revealElement[i].classList.remove("active");
+		}
+	}
+}
+
+window.addEventListener("scroll", scrollReveal);
+
+
+//based on scroll reveal by Miko https://codepen.io/mikomagni/pen/vYNbrYN
